d3aaca1f401fa5d953a42b439032a4c610e206fe
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-10 15:47:44 -0500

    Notación Científica

---
+++ S1-Conceptos-Basicos/NotacionCientifica/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /* *NOTACIÓN CIENTÍFICA* */
+
+    int var1 = 3e3; // 3x10^3
+    double var2 = 5.3e3; // 5.3x10^3
+    double var3 = 1.1234e-3; // 1.1234x10^-3
+    cout << var1 << endl;
+    cout << var2 << endl;
+    cout << var3 << endl;
+
+    return 0;
+}
