6781c2efe98bb0ff9ed914666f1a0bfb3407dafc
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-10 15:30:11 -0500

    Tipos de variables

---
+++ S1-Conceptos-Basicos/TiposDeVariables/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+        TIPOS DE VARIABLES
+        int : Integer : Enteros
+        char : Character : Caracter
+        float : Float : Flotante (numeros reales)
+        double : Double : Dobles (numeros reales, mayor precision)
+        bool : Boolean : Booleano (valores de verdad)
+    */
+
+    // DECLARACIÒN DE VARIABLES
+    int varInt1, varInt2;
+    char varChar1;
+    float varFloat1;
+    double varDouble1;
+    bool varBoolean1;
+
+    /*
+        El nombre de una variable no puede empezar con un numero
+        No se admiten caracteres especiales (solo guion bajo)
+        No pueden haber variables con el mismo nombre
+    */
+
+    return 0;
+}
