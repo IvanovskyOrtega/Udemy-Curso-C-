eb899415dd953ded93e8433482bbf506fd071e50
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-10 16:54:47 -0500

    Entrada por Consola

---
+++ S1-Conceptos-Basicos/EntradaPorConsola/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /**ENTRADA POR CONSOLA**/
+    /** cin : Console Input **/
+    /** >> : Operador de Extracción **/
+    int numero;
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+    cout << "El valor ingresado es: " << numero << endl;
+    return 0;
+}
