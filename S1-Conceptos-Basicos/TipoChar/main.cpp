c14321687e2edb49e536279db82aff1942566b31
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-10 15:58:12 -0500

    Tipo Char

---
+++ S1-Conceptos-Basicos/TipoChar/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /**TIPO CHAR**/
+    /**
+        Peso en Bytes = 1
+        Nº de valores = 256
+        Rango: de -128 a 127
+    **/
+    char letra = 65;
+    //Cambia el valor numérico a Código ASCII
+    cout << letra << endl;
+    return 0;
+}
