1cf8a9e516448f34a67a267f26111b9f89ab7f61
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-10 15:14:52 -0500

    Comentarios en el código

---
+++ S1-Conceptos-Basicos/ComentariosEnElCodigo/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    Autoria del programa
+*/
+
+int main()
+{
+    // Comentario de una sola linea
+    /* Inicio
+        Comentario multilinea
+        Comentario 1
+        Comentario 2
+        Comentario 3
+        ...
+        Comentario n
+    Fin*/
+    cout << "Hola mundo 1!" << endl; //Mensaje de hola mundo 1
+    cout << "Hola mundo 2!" << endl;
+    return 0;
+}
