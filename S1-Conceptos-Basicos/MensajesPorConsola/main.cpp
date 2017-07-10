7ce3a7870fa8ae7cefab0a59b148a33ee623244e
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-10 15:07:50 -0500

    Mensajes por Consola

---
+++ S1-Conceptos-Basicos/MensajesPorConsola/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // << : Operador de insercion
+    /*
+        cout << "Hello world!" << endl;
+        Con << a cout le insertamos el mensaje:
+        Hello World!
+        Posteriormente le insertamos endl, que es
+        el salto de linea.
+        Terminando la instrucciòn con ;
+    */
+    cout << "Hello world 1!" << endl;
+    cout << "Hello world 2!" << endl;
+    cout << "Hello world 1!";
+    cout << "Hello world 2!" << endl;
+    cout << "Hello world 1!\n";
+    cout << "Hello world 2!" << endl;
+    return 0;
+}
