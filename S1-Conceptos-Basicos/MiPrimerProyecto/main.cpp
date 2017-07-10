e800cf88ae2c4ee7afb777615232ad9fd470cafe
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-10 14:43:26 -0500

    Estructura de un programa

---
+++ S1-Conceptos-Basicos/MiPrimerProyecto/main.cpp
@@ -0,0 +1,20 @@
+//Archivos de cabecera
+//#include: directiva de preprocesamiento
+#include <iostream>
+
+/*
+    Namespace: Espacio de nombres
+    Contiene nombres de funciones variables,
+    en std se encuentra cout.
+    Si no lo incluyeramos, tendrìamos que usar:
+    std::cout
+*/
+using namespace std;
+
+//Funcion principal
+int main()
+{
+    // cout: Console Out = Salida por consola
+    cout << "Hello world!" << endl;
+    return 0;
+}
