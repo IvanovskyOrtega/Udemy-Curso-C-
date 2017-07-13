8f594ce33a7fc26a6566976f87e9c98b479f7869
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-12 19:13:12 -0500

    División entera y División real

---
+++ S2-Operaciones-Basicas/DivisionEntera-Real/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /** DIVISIÓN ENTERA Y DIVISIÓN REAL **/
+    /**
+        La división entera siempre se da entre dos valores
+        enteros, el resultado siempre es entero.
+    **/
+    cout << 7/3 << endl;
+    /**
+        La división real se da cuando al menos uno de los
+        dos valores es real (float o double).
+    **/
+    cout << 7.0/3 << endl;
+    return 0;
+}
