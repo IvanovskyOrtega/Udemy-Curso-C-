25e7874ef64193db21b0c561d77097d243e7d4c7
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-12 19:04:45 -0500

    Operadores matemáticos

---
+++ S2-Operaciones-Basicas/OperadoresAritmeticos/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /**
+        OPERADORES MATEMÁTICOS
+        + : SUMA
+        - : RESTA
+        * : MULTIPLICACIÓN
+        / : DIVISIÓN
+        % : MÓDULO
+    **/
+    int suma,resta,mult,division,modulo;
+    int numero1 = 10;
+    int numero2 = 3;
+    suma = numero1 + numero2;
+    resta = numero2 - numero1;
+    mult = numero1 * numero2;
+    division = numero1 / numero2;
+    modulo = numero1 % numero2;
+    cout << suma << endl;
+    cout << resta << endl;
+    cout << mult << endl;
+    cout << division << endl;
+    cout << modulo << endl;
+    return 0;
+}
