d689bdf33632e2f76169fc9306c03da3945c866a
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-12 19:20:14 -0500

    Precedencia Aritmética

---
+++ S2-Operaciones-Basicas/PrecedenciaAritmetica/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /** PRECEDENCIA ARITMÉTICA **/
+    /**
+        1. Operaciones entre paréntesis
+        2. Multiplicación, división y módulo
+        3. Suma y resta
+    **/
+    cout << 14-8/2*3+1 << endl;     /**  3 **/
+    cout << (14-8)/2*3+1 << endl;   /** 10 **/
+    cout << 14-(8/2*3)+1 << endl;   /**  3 **/
+    cout << 14-8/2*(3+1) << endl;   /** -2 **/
+    cout << (14-8)/2*(3+1) << endl; /** 12 **/
+    return 0;
+}
