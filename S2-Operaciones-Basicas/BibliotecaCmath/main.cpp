ef7d4fa867e5159f5bd018d76344952f2090aed8
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-13 17:20:22 -0500

    Biblioteca cmath

---
+++ S2-Operaciones-Basicas/BibliotecaCmath/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>  // math.h
+
+using namespace std;
+
+int main()
+{
+    double potencia = pow(5,5);
+    double raiz = sqrt(100.0);
+    double seno = sin(0.523599);
+    double coseno = cos(1.0472);
+    double tangente = tan(0.785398);
+    cout << "Potencia: " << potencia << endl;
+    cout << "Raiz: " << raiz << endl;
+    cout << "Seno: " << seno << endl;
+    cout << "Coseno: " << coseno << endl;
+    cout << "Tangente: " << tangente << endl;
+    return 0;
+}
