31dfb843553dd1b0c09e3fd3b04420d2858cc03a
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-13 17:38:25 -0500

    Ejercicio 1: Área de un Triángulo

---
+++ S2-Operaciones-Basicas/areaTriangulo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    /** EJERCICIO 1:  **/
+    /**
+        Escriba un programa que nos permita
+        hallar el área de un triángulo.
+    **/
+    double base, altura, area;
+    cout << "Ingrese el tamanio de la base: " << endl;
+    cin >> base;
+    cout << "Ingrese el la altura: " << endl;
+    cin >> altura;
+    area = (base * altura) / 2;
+    cout << "El area del triangulo es: " << area << endl;
+    return 0;
+}
