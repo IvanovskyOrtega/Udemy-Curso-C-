3831ae079d9c4db6818e73fa653e5f6c6f0144d0
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-13 20:15:28 -0500

    Estructura condicional simple

---
+++ S3-Estructuras-Condicionales/estructuraCondicionalSimple.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    /** ESTRUCTURA CONDICIONAL SIMPLE **/
+    /**
+        if : si
+        else : sino
+    **/
+    if(5 < 5){
+        cout << "Entro al if" << endl;
+    }
+    else{
+        cout << "Entro al else" << endl;
+    }
+    return 0;
+}
