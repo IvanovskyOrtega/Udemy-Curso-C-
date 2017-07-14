17574fb1c6db9bf9cae5b4d4ebb734f5d63513f1
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-14 16:06:16 -0500

    Ejercicio 2: Estacionamiento

---
+++ S3-Estructuras-Condicionales/estacionamiento.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    /** EJERCICIO 3 **/
+    /**
+        En un estacionamiento cobran $1.50 por hora o fracción.
+        Diseñe un programa que determine cuanto debe pagar un cliente
+        por el estacionamiento e su vehículo, conociendo el tiempo de
+        estacionamiento en horas y minutos.
+    **/
+    int horas, minutos;
+    float precio = 0.0;
+    cout << "Horas: ";
+    cin >> horas;
+    cout << "Minutos: ";
+    cin >> minutos;
+    if(minutos > 0)
+        horas++;
+    precio = horas * 1.50;
+    cout << "Debe pagar: " << precio << endl;
+    return 0;
+}
