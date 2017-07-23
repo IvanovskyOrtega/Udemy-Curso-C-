cd0c77d1be094cf2fcef6dcbd3b00a069ffd1110
Ivanovsky <ivanovskyortega@gmail.com>
2017-07-23 14:13:19 -0500

    Ejercicio 2: Cantidad de dígitos (corregido)

--- S4-Estructuras-Repetitivas/digitos.cpp
+++ S4-Estructuras-Repetitivas/digitos.cpp
@@ -11,9 +11,13 @@ int main(){
     int numero, digitos = 0;
     cout << "Ingresa un numero: " << endl;
     cin >> numero;
-    while(numero != 0){
-        numero /= 10;
-        digitos++;
+    if(numero == 0)
+        digitos = 1;
+    else{
+        while(numero != 0){
+            numero /= 10;
+            digitos++;
+        }
     }
     cout << "El numero tiene " << digitos << " digitos" << endl;
     return 0;
